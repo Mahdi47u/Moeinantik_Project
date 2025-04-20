706ad913a7e28110e026006862991ab82ac6c4ee
Mahdi47u <samoein51@gmail.com>
2025-04-20 17:09:46 +0330

    Added the UI and the index for the adminstration Also add-migration and update_database

--- MoeinAntik/0_Framework/obj/Debug/net8.0/0_Framework.AssemblyInfo.cs
+++ MoeinAntik/0_Framework/obj/Debug/net8.0/0_Framework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0_Framework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4cf3b025c1658bae1e62ee45a950b9ea9582ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+260915d13b3ff5818fa7028364fcec37e62e3740")]
 [assembly: System.Reflection.AssemblyProductAttribute("0_Framework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0_Framework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
